0a11cb0d67b26e1691a237e85f766a393b31a7a1
panchalhenil01 <panchalhenil447@gmail.com>
2025-08-03 16:23:08 +0530

    Add files via upload

---
+++ FUNCTION.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,ans;
+	clrscr();
+	//printline();
+	printf("\nWelcome To User Defined Function\n");
+	//printline();
+	printf("\nEnter Value : 56 ");
+	scanf("%d",&a);
+	printf("\nEnter value : 45");
+	scanf("%d",&b);
+	//ans=add(a,b);
+	printf("\nAddition : %d",(a,b));
+	printf("\n");
+	//printline();
+	printf("\nEnter Value : 95");
+	scanf("%d",&a);
+	printf("\nEnter Value : 75");
+	scanf("%d",&b);
+	//ans=sub(a,b);
+	printf("\nSubtraction : %d",(a,b));
+	printf("\n");
+	//printline();
+	getch();
+}
+
+
