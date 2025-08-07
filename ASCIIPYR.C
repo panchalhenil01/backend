4250dc342929775e20f09834315507315f57dd59
panchalhenil01 <panchalhenil447@gmail.com>
2025-08-07 12:02:39 +0530

    Add files via upload

--- ASCIIPYR.C
+++ ASCIIPYR.C
@@ -1,22 +1,22 @@
+
 #include<stdio.h>
 #include<conio.h>
 
 void main()
 {
-  int i,j,k;
-  clrscr();
-
-  for(i=65;i<74;i++)
-  {
-	for(k=0;k<74-i;k++)
+	int i,j,k;
+	clrscr();
+	for(i=65;i<74;i++)
 	{
-		printf(" ");
+		for(k=0;k<74-1;k++)
+		{
+			printf(" ");
+		}
+		for(j=65;j<i;j++)
+		{
+			printf("%c",i);
+		}
+		printf("\n");
 	}
-	for(j=65;j<i;j++)
-	{
-		printf(" %c",i);
-	}
-	printf("\n");
-  }
-  getch();
+	getch();
 }
