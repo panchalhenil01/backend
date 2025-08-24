0a167496056f97727ed566a0578719084363ad27
panchalhenil01 <panchalhenil447@gmail.com>
2025-08-24 11:36:36 +0530

    Add files via upload

---
+++ SWAPINGV.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int no1,no2,temp;
+	clrscr();
+	printf("\nEnter the number : ");
+	scanf("%d",&no1);
+	printf("\nEnter the number : ");
+	scanf("%d",&no2);
+	{
+		temp = no;
+		no1=no2;
+		no2=temp;
+	}
+	printf("\nSwaping Number no1 = %d and no2 =%d",no1,no2);
+	getch();
+}
