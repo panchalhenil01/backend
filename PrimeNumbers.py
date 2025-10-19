70266663fb881a4ba701ae5d502aeb018d35fed5
panchalhenil01 <panchalhenil447@gmail.com>
2025-10-19 20:01:30 +0530

    Add files via upload

---
+++ PrimeNumbers.py
@@ -0,0 +1,16 @@
+n=int(input("Enter Numbers : "))
+
+if n%2!=0:
+    for i in range(3,int(n/2)+1,2):
+        if n%i==0:
+            print(n," Is Not Prime")
+            break
+    else:
+        print(n," Is Prime")
+else:
+    print(n," Is Not Prime")
+        
+                   
+    
+    
+
