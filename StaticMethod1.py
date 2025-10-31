be631c5a656fa5b16deee192b1be11ca5939b5ab
panchalhenil01 <panchalhenil447@gmail.com>
2025-10-31 21:38:10 +0530

    Add files via upload

---
+++ StaticMethod1.py
@@ -0,0 +1,12 @@
+class Calculator:
+
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @staticmethod
+    def subtraction(x, y):
+        return x - y
+
+print(Calculator.add(5, 3))
+print(Calculator.subtraction(5, 3))
