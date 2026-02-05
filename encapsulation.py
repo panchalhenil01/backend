c9846c33a0d423dae31eea4381d2be4f0d7407a6
panchalhenil01 <panchalhenil447@gmail.com>
2026-02-05 19:49:15 +0530

    Add files via upload

---
+++ encapsulation.py
@@ -0,0 +1,13 @@
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def get_balance(self):
+        return self.__balance
+
+    def set_balance(self, amount):
+        if amount >= 0:
+            self.__balance = amount
+
+account = BankAccount(5000)
+print(account.get_balance())
